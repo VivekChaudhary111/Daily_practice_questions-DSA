13b2817e3d9e170fb778303e01d73d4f2a4c89ca
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2025-08-10 21:05:24 +0530

    Update using bit manipulation

--- Leetcode 231 | Power of Two.java
+++ Leetcode 231 | Power of Two.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n <= 0){
-            return false;
-        }
-        while(n % 2 == 0){
-            n /= 2;
-        }
-        if(n == 1) return true;
-        return false;
+        // if(n <= 0){
+        //     return false;
+        // }
+        // while(n % 2 == 0){
+        //     n /= 2;
+        // }
+        // if(n == 1) return true;
+        // return false;
+
+        // bit manipulation
+        if(n <= 0) return false;
+        return (n & n-1) == 0;
     }
 }
