8cb3f045cac658631ac27b37d09576f04cb1e68d
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2025-08-13 23:34:15 +0530

    Create Leetcode 968 | Binary Tree Cameras.java

---
+++ Leetcode 968 | Binary Tree Cameras.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ class Solution {
+			  int camera = 0;
+			  public int minCameraCover(TreeNode root) {
+				  int c = minCamera(root);
+				  if(c == -1) {
+					  camera++;
+				  }
+				  return camera;
+			        
+			  }
+			  public int minCamera(TreeNode root) {
+				  	if(root == null) {
+				  		return 0;
+				  	}
+			        int left = minCamera(root.left);
+			        int right = minCamera(root.right);
+			        if(left == -1 || right == -1) {  // is node ko camera ki need hai
+			        	camera++;
+			        	return 1;  // camera setup kra hai iss node pe
+			        }
+			        else if(left == 1 || right == 1) {  // inme se ksi ek pass camera h ya dono ke pass h
+			        	return 0;  // iska matlab main covered hun
+			        }else {
+			        	return -1;  // need a camera
+			        }
+			        	
+			  }
+		}
