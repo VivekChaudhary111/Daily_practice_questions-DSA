3a203ff8e411225573b834bf2b9d58bf2a833497
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2025-09-26 17:38:41 +0530

    File updated at 9/26/2025, 5:38:39 PM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,15 +1,16 @@
-// Last updated: 9/26/2025, 5:23:01 PM
+// Last updated: 9/26/2025, 5:38:39 PM
 class Solution {
     public void moveZeroes(int[] nums) {
-        int k = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != 0){
-                nums[k] = nums[i];
-                k++;
+        int point = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[point] = nums[i];
+                point++;
             }
         }
-        for(int i = k; i < nums.length; i++){
-            nums[i] = 0;
+        while(point < nums.length) {
+            nums[point] = 0;
+            point++;
         }
     }
 }
