47d08f4b81f7a744eb1832aae3850745d13f9274
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2025-09-28 17:46:32 +0530

    File updated at 9/28/2025, 5:46:31 PM

--- 976-LargestPerimeterTriangle/976-LargestPerimeterTriangle.java
+++ 976-LargestPerimeterTriangle/976-LargestPerimeterTriangle.java
@@ -1,14 +1,13 @@
-// Last updated: 9/28/2025, 5:40:19 PM
+// Last updated: 9/28/2025, 5:46:31 PM
 class Solution {
     public int largestPerimeter(int[] nums) {
         int n = nums.length;
-        int ans = 0;
         Arrays.sort(nums);
-        for(int i = 0; i<n-2; i++){
-            if(nums[i]+nums[i+1] > nums[i+2]){
-                ans = Math.max(ans, nums[i]+nums[i+1]+nums[i+2]);
+        for(int i = n-1; i>=2; i--){
+            if(nums[i-2]+nums[i-1] > nums[i]){
+                return nums[i-2]+nums[i-1]+nums[i];
             }
         }
-        return ans;
+        return 0;
     }
 }
