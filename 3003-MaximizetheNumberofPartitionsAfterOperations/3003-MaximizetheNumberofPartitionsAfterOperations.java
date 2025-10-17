280ef22da8e533f831c53119bc22d18772e7ba8c
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2025-10-17 19:12:36 +0530

    File updated at 10/17/2025, 7:12:31 PM

--- 3003-MaximizetheNumberofPartitionsAfterOperations/3003-MaximizetheNumberofPartitionsAfterOperations.java
+++ 3003-MaximizetheNumberofPartitionsAfterOperations/3003-MaximizetheNumberofPartitionsAfterOperations.java
@@ -1,37 +1,41 @@
-// Last updated: 10/17/2025, 7:11:28 PM
-import java.util.*;
-
+// Last updated: 10/17/2025, 7:12:31 PM
 class Solution {
-    public int maxPartitionsAfterOperations(String s, int k) {
-        Map<Long, Integer> memo = new HashMap<>();
-        return dfs(s, 0, true, 0, k, memo) + 1;
-    }
-
-    private int dfs(String s, int i, boolean canChange, int mask, int k, Map<Long, Integer> memo) {
-        if (i == s.length()) return 0;
-
-        long key = ((long) i << 27) | ((canChange ? 1L : 0L) << 26) | mask;
-        if (memo.containsKey(key)) return memo.get(key);
-
-        int res = helper(s, i, canChange, mask, 1 << (s.charAt(i) - 'a'), k, memo);
-
-        if (canChange) {
-            for (int c = 0; c < 26; ++c) {
-                res = Math.max(res, helper(s, i, false, mask, 1 << c, k, memo));
+    public int maxPartitionsAfterOperations(String S, int k) {
+        if(k == 26) return 1;
+        char[] s = S.toCharArray();
+        int n = s.length;
+        int[] left1 = new int[n], left2 = new int[n];
+        int[] partitions = new int[n];
+        int mask1 = 0, mask2 = 0, count = 1;
+        for(int i = 0; i < n; i++) {
+            int filter = 1 << (s[i] - 'a');
+            mask2 |= mask1 & filter;
+            mask1 |= filter;
+            if(Integer.bitCount(mask1) > k) {
+                mask1 = filter;
+                mask2 = 0;
+                count++;
+            }
+            left1[i] = mask1;
+            left2[i] = mask2;
+            partitions[i] = count;
+        }
+        int ans = count;
+        mask1 = mask2 = count = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            int filter = 1 << (s[i] - 'a');
+            mask2 |= mask1 & filter;
+            mask1 |= filter;
+            if(Integer.bitCount(mask1) > k) {
+                mask1 = filter;
+                mask2 = 0;
+                count++;
+            }
+            if(Integer.bitCount(mask1) == k) {
+                if((filter & mask2) != 0 && (filter & left2[i]) != 0 && Integer.bitCount(left1[i]) == k && (left1[i] | mask1) != (1 << 26) - 1) ans = Math.max(ans, count + partitions[i] + 2);
+                else if(mask2 != 0) ans = Math.max(ans, count + partitions[i] + 1);
             }
         }
-
-        memo.put(key, res);
-        return res;
-    }
-
-    private int helper(String s, int i, boolean canChange, int mask, int newBit, int k, Map<Long, Integer> memo) {
-        int newMask = mask | newBit;
-
-        if (Integer.bitCount(newMask) > k) {
-            return 1 + dfs(s, i + 1, canChange, newBit, k, memo);
-        } else {
-            return dfs(s, i + 1, canChange, newMask, k, memo);
-        }
+        return ans;
     }
 }
