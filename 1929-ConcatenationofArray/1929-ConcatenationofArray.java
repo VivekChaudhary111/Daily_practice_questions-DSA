6579450edc761e3b7b4c71612ad486f0a0ef4a1c
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2025-11-14 12:09:05 +0530

    File updated at 11/14/2025, 12:09:01 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.java
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.java
@@ -1,13 +1,11 @@
-// Last updated: 11/14/2025, 12:06:18 PM
+// Last updated: 11/14/2025, 12:09:01 PM
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int[] ans = new int[2*(nums.length)];
-        int j = 0;
-        for(int i = 0; i < 2*(nums.length); i++){
-            ans[i] = nums[j++];
-            if(j == nums.length){
-                j = 0;
-            }
+        int n = nums.length;
+        int[] ans = new int[2*n];
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
         }
         return ans;
     }
