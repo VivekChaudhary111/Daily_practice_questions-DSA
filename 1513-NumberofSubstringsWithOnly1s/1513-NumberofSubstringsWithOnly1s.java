33f8bd3555daadda7034c8f7b523f4e94c3e119f
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2025-11-16 12:54:31 +0530

    File updated at 11/16/2025, 12:54:26 PM

--- 1513-NumberofSubstringsWithOnly1s/1513-NumberofSubstringsWithOnly1s.java
+++ 1513-NumberofSubstringsWithOnly1s/1513-NumberofSubstringsWithOnly1s.java
@@ -1,2 +1,17 @@
-// Last updated: 11/16/2025, 12:50:46 PM
-long subans = (long) key * (key + 1) / 2 * map.get(key);
+// Last updated: 11/16/2025, 12:54:26 PM
+class Solution {
+    static int mod=(int)Math.pow(10,9)+7;
+    public int numSub(String s) {
+       int cnt = 0;
+       int j=0;
+       for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='0'){
+            j=i+1;
+        }
+        else{
+            cnt=(cnt+(i-j+1))%mod;
+        }
+       }
+       return cnt%mod;
+    }
+}
