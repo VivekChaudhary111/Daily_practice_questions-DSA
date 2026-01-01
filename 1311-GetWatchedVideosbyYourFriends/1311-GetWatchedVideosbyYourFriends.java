cb22da36a891a81b9ebbd4cacff9b6f67bbc5080
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-01-01 19:38:18 +0530

    File updated at 1/1/2026, 7:38:17 PM

--- 1311-GetWatchedVideosbyYourFriends/1311-GetWatchedVideosbyYourFriends.java
+++ 1311-GetWatchedVideosbyYourFriends/1311-GetWatchedVideosbyYourFriends.java
@@ -1,4 +1,4 @@
-// Last updated: 1/1/2026, 7:34:13 PM
+// Last updated: 1/1/2026, 7:38:17 PM
 1class Solution {
 2    public List<String> watchedVideosByFriends(List<List<String>> watchedVideos, int[][] friends, int id, int level) {
 3        int n = friends.length;
@@ -21,32 +21,33 @@
 20            vis.add(rp.vtx);
 21            if(rp.cur_level == level){
 22                isthis[rp.vtx] = true;
-23            }
-24            for(int nbrs: graph.get(rp.vtx)){
-25                if(!vis.contains(nbrs)){
-26                    q.add(new Pair(nbrs, rp.cur_level + 1));
-27                }
-28            }
-29        }
-30        Map<String, Integer> ans = new HashMap<>();
-31        for(int i = 0 ; i < n; i++){
-32            if(isthis[i]){
-33                for(String video: watchedVideos.get(i)){
-34                    ans.put(video, ans.getOrDefault(video, 0) + 1);
-35                }
-36            }
-37        }
-38        List<String> final_ans = new ArrayList<>(ans.keySet());
-39        final_ans.sort((a, b)-> ans.get(a) == ans.get(b) ? a.compareTo(b) : ans.get(a)-ans.get(b));
-40        return final_ans;
-41        
-42    }
-43    public class Pair{
-44        int vtx;
-45        int cur_level;
-46        public Pair(int vtx, int cur_level){
-47            this.vtx = vtx;
-48            this.cur_level = cur_level;
-49        }
-50    }
-51}
+23                continue;
+24            }
+25            for(int nbrs: graph.get(rp.vtx)){
+26                if(!vis.contains(nbrs)){
+27                    q.add(new Pair(nbrs, rp.cur_level + 1));
+28                }
+29            }
+30        }
+31        Map<String, Integer> ans = new HashMap<>();
+32        for(int i = 0 ; i < n; i++){
+33            if(isthis[i]){
+34                for(String video: watchedVideos.get(i)){
+35                    ans.put(video, ans.getOrDefault(video, 0) + 1);
+36                }
+37            }
+38        }
+39        List<String> final_ans = new ArrayList<>(ans.keySet());
+40        final_ans.sort((a, b)-> ans.get(a) == ans.get(b) ? a.compareTo(b) : ans.get(a)-ans.get(b));
+41        return final_ans;
+42        
+43    }
+44    public class Pair{
+45        int vtx;
+46        int cur_level;
+47        public Pair(int vtx, int cur_level){
+48            this.vtx = vtx;
+49            this.cur_level = cur_level;
+50        }
+51    }
+52}
