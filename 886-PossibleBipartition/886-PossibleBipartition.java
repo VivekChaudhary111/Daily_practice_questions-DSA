a558fe7584878d586f4363ea3eaecb195b6a0eb5
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-01-03 23:43:46 +0530

    File updated at 1/3/2026, 11:43:43 PM

--- 886-PossibleBipartition/886-PossibleBipartition.java
+++ 886-PossibleBipartition/886-PossibleBipartition.java
@@ -1,4 +1,4 @@
-// Last updated: 1/3/2026, 11:42:28 PM
+// Last updated: 1/3/2026, 11:43:43 PM
 1class Solution {
 2    public boolean possibleBipartition(int n, int[][] dislikes) {
 3        List<List<Integer>> graph = new ArrayList<>();
