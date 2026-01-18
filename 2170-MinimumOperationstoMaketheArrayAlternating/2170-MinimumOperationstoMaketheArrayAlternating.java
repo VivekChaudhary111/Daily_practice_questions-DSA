926f0a1d66c588c5cbde56706d962d822985cfd7
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-01-18 22:38:54 +0530

    Create 2170-MinimumOperationstoMaketheArrayAlternating

---
+++ 2170-MinimumOperationstoMaketheArrayAlternating/2170-MinimumOperationstoMaketheArrayAlternating.java
@@ -0,0 +1,17 @@
+// Last updated: 1/18/2026, 10:38:55 PM
+1class Solution {
+2    public int minimumOperations(int[] nums) {
+3        int freq[][] = new int[100005][2];
+4        int i, j, k, ans=0;
+5        for(i = 0; i < nums.length; i++) {
+6    			freq[nums[i]][i&1]++;
+7    		}
+8    		
+9    		for(i = 1, j=k=0; i <= 100000; i++) {
+10    		ans = Math.max(ans, Math.max(freq[i][0] + k, freq[i][1] + j));
+11            j = Math.max(j, freq[i][0]);
+12            k = Math.max(k, freq[i][1]);
+13        }
+14        return nums.length - ans;
+15    }
+16}
