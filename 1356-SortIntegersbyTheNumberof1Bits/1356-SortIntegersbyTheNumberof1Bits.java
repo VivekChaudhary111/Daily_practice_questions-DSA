c5baefcfe7c82f3926064cf84fd36e781f3f797d
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-02-26 03:58:14 +0530

    File updated at 2/26/2026, 3:58:09 AM

--- 1356-SortIntegersbyTheNumberof1Bits/1356-SortIntegersbyTheNumberof1Bits.java
+++ 1356-SortIntegersbyTheNumberof1Bits/1356-SortIntegersbyTheNumberof1Bits.java
@@ -1,12 +1,21 @@
-// Last updated: 2/26/2026, 3:52:40 AM
+// Last updated: 2/26/2026, 3:58:09 AM
 1class Solution {
 2    public int[] sortByBits(int[] arr) {
-3        Arrays.sort(arr);
-4       List<Integer> list=new ArrayList<>();
-5        for(int val:arr)list.add(val);
-6        list.sort((a,b)->Integer.bitCount(a)-Integer.bitCount(b));
-7//        return list.stream().toArray();
-8        for(int i=0;i<arr.length;i++)arr[i]=list.get(i);
-9        return arr;
-10    }
-11}
+3        int a[][] = new int[arr.length][2];
+4        int b[] = new int[arr.length];
+5        for (int i = 0; i < arr.length; i++) {
+6            a[i][0] = arr[i];
+7            a[i][1] = Integer.bitCount(arr[i]);
+8        }
+9        Arrays.sort(a, (c, d) ->{
+10            if(c[1]==d[1]){
+11                return Integer.compare(c[0],d[0]);
+12            }
+13        return Integer.compare(c[1], d[1]);});
+14        
+15        for (int j = 0; j < arr.length; j++) {
+16            arr[j] = a[j][0];
+17        }
+18        return arr;
+19    }
+20}
