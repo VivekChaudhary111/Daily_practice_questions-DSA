2820468dfa326ce7a0ead5c1a1a6806bf9ae707d
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-02-27 02:20:03 +0530

    File updated at 2/27/2026, 2:19:58 AM

--- 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.java
+++ 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.java
@@ -1,22 +1,19 @@
-// Last updated: 2/27/2026, 2:19:01 AM
+// Last updated: 2/27/2026, 2:19:58 AM
 1public class Solution {
 2
 3    public int numSteps(String s) {
-4        int N = s.length();
-5
-6        int operations = 0;
-7        int carry = 0;
-8        for (int i = N - 1; i > 0; i--) {
-9            int digit = (s.charAt(i)-'0') + carry;
-10
-11            if (digit % 2 == 1) {
-12                operations += 2;
-13                carry = 1;
-14            } else {
-15                operations++;
-16            }
-17        }
-18
-19        return operations + carry;
-20    }
-21}
+4        int n = s.length();
+5        int ops = 0;
+6        int carry = 0;
+7        for (int i = n - 1; i > 0; i--) {
+8            int digit = (s.charAt(i)-'0') + carry;
+9            if (digit % 2 == 1) {
+10                ops += 2;
+11                carry = 1;
+12            } else {
+13                ops++;
+14            }
+15        }
+16        return ops + carry;
+17    }
+18}
