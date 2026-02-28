93db38a53f340416e6998da67b946d2a4b890d58
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-03-01 01:31:00 +0530

    File updated at 3/1/2026, 1:30:59 AM

--- 1680-ConcatenationofConsecutiveBinaryNumbers/1680-ConcatenationofConsecutiveBinaryNumbers.java
+++ 1680-ConcatenationofConsecutiveBinaryNumbers/1680-ConcatenationofConsecutiveBinaryNumbers.java
@@ -1,17 +1,22 @@
-// Last updated: 3/1/2026, 1:29:56 AM
+// Last updated: 3/1/2026, 1:30:59 AM
 1class Solution {
 2    public int concatenatedBinary(int n) {
-3        long res = 0;
-4        int MOD = 1_000_000_007;
-5        int len = 0;
-6
-7        for (int i = 1; i <= n; i++) {
-8            if ((i & (i - 1)) == 0) {
-9                len++;
-10            }
-11            res = ((res << len) % MOD + i) % MOD;
-12        }
-13
-14        return (int) res;
-15    }
-16}
+3        int mod = 1_000_000_007;
+4
+5        long ans=0;
+6        for(int i=1;i<=n;i++){
+7            ans = ((ans<<len(i))%mod+i)%mod;
+8        }
+9
+10        return (int)ans;
+11    }
+12
+13    public int len(int n){
+14        int l=0;
+15        while(n>0){
+16            l++;
+17            n=(n>>1);
+18        }
+19        return l;
+20    }
+21}
