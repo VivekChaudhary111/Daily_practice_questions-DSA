6cfa9e807795e0cf7f3199fd2209b49755952a54
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-03-16 16:35:44 +0530

    File updated at 3/16/2026, 4:35:43 PM

--- 40-CombinationSumII/40-CombinationSumII.java
+++ 40-CombinationSumII/40-CombinationSumII.java
@@ -1,4 +1,4 @@
-// Last updated: 3/16/2026, 4:35:10 PM
+// Last updated: 3/16/2026, 4:35:43 PM
 1class Solution {
 2    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
 3        List<List<Integer>> ans = new ArrayList<>();
@@ -12,12 +12,13 @@
 11            ans.add(new ArrayList<>(subans));
 12            return;
 13        }
-14        if(idx == candidates.length || target < 0) return;
+14        if(idx == candidates.length) return;
 15        for(int i = idx; i < candidates.length; i++){
-16            if(i > idx && candidates[i] == candidates[i-1]) continue;
-17            subans.add(candidates[i]);
-18            solve(candidates, target - candidates[i], i+1, ans, subans);
-19            subans.remove(subans.size() - 1);
-20        }
-21    }
-22}
+16            if(target - candidates[i] < 0) break;
+17            if(i > idx && candidates[i] == candidates[i-1]) continue;
+18            subans.add(candidates[i]);
+19            solve(candidates, target - candidates[i], i+1, ans, subans);
+20            subans.remove(subans.size() - 1);
+21        }
+22    }
+23}
