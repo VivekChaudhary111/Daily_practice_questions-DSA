b4859683cff0131307c3e743338da86b6bfe0896
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-04-20 08:03:43 +0530

    File updated at 4/20/2026, 8:03:42 AM

--- 2078-TwoFurthestHousesWithDifferentColors/2078-TwoFurthestHousesWithDifferentColors.java
+++ 2078-TwoFurthestHousesWithDifferentColors/2078-TwoFurthestHousesWithDifferentColors.java
@@ -1,15 +1,22 @@
-// Last updated: 4/20/2026, 7:58:49 AM
-1class Solution {
-2    public int maxDistance(int[] colors) {
-3        int ans = 0;
-4        for(int i = 0; i < colors.length; i++){
-5            for(int j = colors.length -1; j >= 0; j--){
-6                if(colors[i] != colors[j]){
-7                    ans = Math.max(ans, j - i);
-8                    break;
-9                }
-10            }
-11        }
-12        return ans;
-13    }
-14}
+// Last updated: 4/20/2026, 8:03:42 AM
+class Solution {
+    public int maxDistance(int[] colors) {
+       int dis = 0;
+       int n = colors.length;
+       for(int i=0;i<n-1;i++){
+        if(colors[i] != colors[n-1]){
+            dis = Math.max(dis ,(n-1)-i);
+            break;
+        }
+       }
+      
+
+       for(int i=n-1;i>=1;i--){
+        if(colors[0]!=colors[i]){
+        dis = Math.max(dis,i);
+       }
+       }
+       return dis;
+    
+    }
+}
