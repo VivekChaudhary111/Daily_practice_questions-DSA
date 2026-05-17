77f2abc7af7ae86b64ca631b5b0389db98af3b97
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-05-17 11:27:01 +0530

    File updated at 5/17/2026, 11:26:57 AM

--- 1306-JumpGameIII/1306-JumpGameIII.java
+++ 1306-JumpGameIII/1306-JumpGameIII.java
@@ -1,16 +1,16 @@
-// Last updated: 5/17/2026, 11:22:31 AM
+// Last updated: 5/17/2026, 11:26:57 AM
 1class Solution {
 2    public boolean canReach(int[] arr, int start) {
 3        Queue<Integer> q = new LinkedList<>();
-4        Set<Integer> set = new HashSet<>();
+4        boolean[] set = new boolean[arr.length];
 5        q.add(start);
 6        while(!q.isEmpty()){
 7            int r = q.poll();
-8            if(set.contains(r)) continue;
-9            set.add(r);
+8            if(set[r]) continue;
+9            set[r] = true;
 10            if(arr[r] == 0) return true;
-11            if(r + arr[r] < arr.length && !set.contains(r + arr[r])) q.add(r + arr[r]);
-12            if(r - arr[r] >= 0 && !set.contains(r - arr[r])) q.add(r - arr[r]);
+11            if(r + arr[r] < arr.length) q.add(r + arr[r]);
+12            if(r - arr[r] >= 0) q.add(r - arr[r]);
 13        }
 14        return false;
 15    }
