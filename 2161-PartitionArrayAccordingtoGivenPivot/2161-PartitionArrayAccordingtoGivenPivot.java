9500ee4da92197a89da6d5b550c1cc749695cbd7
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-06-10 23:55:33 +0530

    File updated at 6/10/2026, 11:55:24 PM

--- 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.java
+++ 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.java
@@ -1,4 +1,4 @@
-// Last updated: 6/10/2026, 11:54:24 PM
+// Last updated: 6/10/2026, 11:55:24 PM
 1class Solution {
 2    public long maxTotalValue(int[] nums, int k) {
 3        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
