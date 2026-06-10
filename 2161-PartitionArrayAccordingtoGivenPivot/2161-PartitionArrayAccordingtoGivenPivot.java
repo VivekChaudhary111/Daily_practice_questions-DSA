0d461e5b458b878856e29d78b72c595525d129e2
Vivek Chaudhary <142593464+VivekChaudhary111@users.noreply.github.com>
2026-06-10 23:54:33 +0530

    Create 2161-PartitionArrayAccordingtoGivenPivot

---
+++ 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.java
@@ -0,0 +1,12 @@
+// Last updated: 6/10/2026, 11:54:24 PM
+1class Solution {
+2    public long maxTotalValue(int[] nums, int k) {
+3        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+4        for(int num: nums){
+5            if(num < min) min = num;
+6            if(num > max) max = num;
+7        }
+8        long ans = (long) k*(max-min);
+9        return ans;
+10    }
+11}
